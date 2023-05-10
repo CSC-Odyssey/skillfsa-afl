171c2b000ebb2172b1c5bbd501407f140edac7a1
Bryan Capistrano <89622671+capistrano-bryan@users.noreply.github.com>
2023-05-10 22:03:45 +0800

    Update  script.js
    Fixed some CSS problems
    Added a function that will check for the skillset and update the check marks in web application

--- DevHire Website/script.js
+++ DevHire Website/script.js
@@ -1,54 +1,58 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // console.log("DOMContentLoaded event fired!");
-    // var toggleButton = document.getElementById("myButton");
-    // toggleButton.addEventListener('click', hideDiv);
-
     var getDataButton = document.getElementById("myButton2");
     getDataButton.addEventListener('click', getData());
     getDataButton.style.display = "none";
 
-    // function hideDiv() {
-    //     var myDiv = document.getElementById("html-square");
-    //     if (myDiv.style.display !== "none"){
-    //         myDiv.style.display = "none";
-    //     } else {
-    //         myDiv.style.display = "block";
-    //     }
-    // }
-
     function getData() {
-        console.log("getData event fired!");
+      console.log("getData event fired!");
 
-        const blockElement = document.querySelector('#block-element');
-        fetch('../pdf/result.json')
-            .then(response => response.json())
-            .then(data => {
-                data.results.forEach(item => {
-                    const htmlString = {
-                        template: `
-                            <div class="title">
-                              <div class="title-container">
-                                <p>\${item.name}</p>
-                                <p>\${item.email}</p>
-                              </div>
-                              <div class="square-container">
-                                <div id="html-square"class="square" style="background-color: #418BD0;"></div>
-                                <div id="css-square" class="square" style="background-color: #D04197;"></div>
-                                <div id="js-square" class="square" style="background-color: #D07541;"></div>
-                              </div>
-                            </div>
-                        `
-                        };
-                    const newHTML = htmlString.template.replace(/\${item.name}/g, item.name).replace(/\${item.email}/g, item.email);
+      const blockElement = document.querySelector('#block-element');
+      fetch('../pdf/result.json')
+          .then(response => response.json())
+          .then(data => {
+              data.results.forEach(item => {
+                let blue = '<div id="tick-mark-blue"></div>';
+                let pink = '<div id="tick-mark-pink"></div>';
+                let orange = '<div id="tick-mark-orange"></div>';
 
-                    const container = document.createElement('article');
-                    container.classList.add('block');
-                    container.classList.add('block-width');
-                    container.innerHTML = newHTML;
+                if (!item.skillset.includes('HTML')) {
+                  console.log("one")
+                  blue = '<div id="tick-mark-blue" style="display: none"></div>';
+                }
+                if (!item.skillset.includes('CSS')) {
+                  console.log("two")
+                  pink = '<div id="tick-mark-pink" style="display: none"></div>';
+                }
+                if (!item.skillset.includes('JavaScript')) {
+                  console.log("three")
+                  orange = '<div id="tick-mark-orange" style="display: none"></div>';
+                }
 
-                    blockElement.insertAdjacentElement('beforeend', container);
-                });
-            })
-            .catch(error => console.error(error));
+                const htmlString = {
+                    template: `
+                        <div class="title">
+                          <div class="title-container">
+                            <p>\${item.name}</p>
+                            <p>\${item.email}</p>
+                          </div>
+                          <div class="square-container">
+                            ${blue}
+                            ${pink}
+                            ${orange}
+                          </div>
+                        </div>
+                    `
+                    };
+                const newHTML = htmlString.template.replace(/\${item.name}/g, item.name).replace(/\${item.email}/g, item.email);
+
+                const container = document.createElement('article');
+                container.classList.add('block');
+                container.classList.add('block-width');
+                container.innerHTML = newHTML;
+
+                blockElement.insertAdjacentElement('beforeend', container);
+              });
+          })
+          .catch(error => console.error(error));
     }
 }); 
