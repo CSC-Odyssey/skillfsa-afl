2402858e96af7bb060a8358482d09a99800f1bc5
JAustin25 <austinandres36@gmail.com>
2023-05-14 16:13:22 +0800

    [Update] minor css (results.php)

--- DevHire Website/results.php
+++ DevHire Website/results.php
@@ -7,22 +7,24 @@ include("resultsHandler.php");
 <head>
 	<script src="results.js"></script>
 	<script src="index.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<style src="style.css"></style>
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=VT323">
-	<a href="index.php"><button class="button">UPLOAD</button></a>
+	<a href="index.php"><button class="button"><i class="fa fa-home"></i> HOME</button></a>
+
 	<style>
 		      .button {
         background-color: #B05454;
         color: #D9D9D9;
         border: none;
-        border-radius: 5px;	
+        border-radius: 20px;	
         font-size: 25px;
         font-family: "DotGothic16", monospace;
-        padding: 10px 80px;
+        padding: 10px 40px;
         margin-top: 20px;
-		margin-left: 1150px;
+		margin-left: 1100px;
         cursor: pointer;
         white-space: nowrap;
 		letter-spacing: 5px;
